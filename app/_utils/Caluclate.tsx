679686866ad7f95fd6b4cb2d48780b1a6219b7a4
sachinmmane <tosachinmane@gmail.com>
2024-06-29 18:31:06 +0530

    feat: extended calculate function to handle any number of numbers and added corresponding test cases

--- app/_utils/Caluclate.tsx
+++ app/_utils/Caluclate.tsx
@@ -1,3 +1,9 @@
 export const calculate = (inputValue: string): number | undefined => {
   if (inputValue === "") return 0;
+  const numbers = inputValue
+    .split(",")
+    .map((numStr) => parseInt(numStr.trim(), 10));
+  const sum = numbers.reduce((acc, num) => acc + num, 0);
+
+  return sum;
 };
