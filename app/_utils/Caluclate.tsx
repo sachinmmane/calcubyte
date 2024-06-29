bd6369aba5ea0f58f3edb63b1a10b3d517da9ff0
sachinmmane <tosachinmane@gmail.com>
2024-06-29 20:18:40 +0530

    feat: extended calculate function to handle custom delimiters with //[delimiter]n[numbers…]. example: //

--- app/_utils/Caluclate.tsx
+++ app/_utils/Caluclate.tsx
@@ -1,11 +1,26 @@
 export const calculate = (inputValue: string): number | undefined => {
   if (inputValue === "") return 0;
+
+  let delimiter = ","; // Default delimiter
+  let numbersString = inputValue;
+
+  // Check if the input starts with a custom delimiter definition
+  if (inputValue.startsWith("//")) {
+    // Extract delimiter from the input
+    const delimiterMatch = inputValue.match(/^\/\/(.*)\n/);
+    if (delimiterMatch && delimiterMatch[1]) {
+      delimiter = delimiterMatch[1];
+      // Remove the delimiter definition from the input string
+      numbersString = inputValue.substring(delimiterMatch[0].length);
+    }
+  }
+
   // Normalize input: replace newlines with commas
-  const normalizedInput = inputValue.replace(/\n/g, ",");
+  const normalizedInput = numbersString.replace(/\n/g, delimiter);
 
   // Split the normalized input string by commas, trim each segment, then parse into integers
   const numbers = normalizedInput
-    .split(",")
+    .split(delimiter)
     .map((numStr) => parseInt(numStr.trim(), 10));
 
   // Calculate the sum of the parsed numbers
