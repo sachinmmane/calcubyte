e021c5edb9eb893dc6add40811b54c1a9ee40cab
sachinmmane <tosachinmane@gmail.com>
2024-07-01 11:59:05 +0530

    chore: added calcubyte logo

--- app/_components/navbar.tsx
+++ app/_components/navbar.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHistory } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
+import logo from "/public/logo.png";
 interface NavbarProps {
   onToggleSidebar: () => void;
 }
@@ -10,7 +12,7 @@ const Navbar: React.FC<NavbarProps> = ({ onToggleSidebar }) => {
     <nav className="p-4 fixed w-full z-10">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-3xl font-bold absolute left-auto transform -translate-x-1/2">
-          calcubyte
+          <Image src={logo} alt="Calcubyte Logo" width={150} height={40} />{" "}
         </div>
         <div
           className={`flex transition-all duration-300 justify-end w-full ml-auto`}
